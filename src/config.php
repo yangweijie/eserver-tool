48ef4880299d15670d0efc403b819c4fff527ae8
yangweijie <917647288@qq.com>
2024-05-27 16:36:17 +0800

    添加jq 目录读取配置 搜索完成

---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+// 2024-05-27 07:46:56 更新
+return array (
+  'dir' => 'D:\\GreenSoft\\Eserver',
+);
--- src/index.php
+++ src/index.php
@@ -5,6 +5,26 @@ require_once "vendor/autoload.php";
 use KingBes\PhpWebview\WebView;
 use KingBes\PhpWebview\Dialog;
 
+define('DS', DIRECTORY_SEPARATOR);
+function config($key='', $default = null){
+    $conf = require 'config.php';
+    return $key? ($conf[$key]??$default) : $conf;
+}
+
+function config_save($dir){
+    $now = date("Y-m-d H:i:s");
+    $configs = var_export([
+        'dir'=>$dir
+    ], true);
+    $content = <<<Conf
+<?php
+// $now 更新
+return $configs;
+Conf;
+
+    file_put_contents('config.php', $content);
+}
+
 function result($code, $msg, $data = []){
     return ["code" => $code, "msg" => $msg, "data" => $data];
 }
@@ -18,19 +38,40 @@ function error($msg, $data=[]){
 }
 
 // 实例
-$webview = new WebView('Php WebView', 640, 480, true, __DIR__);
+$webview = new WebView('Eserver 工具箱', 640, 480, true, __DIR__);
 $view = 'file:///'.realpath('home.html');
 // 获取html
 // $html = file_get_contents(__DIR__ . '/src/index.html');
 $webview->navigate($view);
 // 绑定
-$webview->bind('btn', function ($seq, $req, $context) {
-    return $req;
+function parse_apps(string $software_file)
+{
+    if(is_file($software_file)){
+        return json_decode(file_get_contents($software_file), true);
+    }
+    return [];
+}
+
+$webview->bind('apps', function ($seq, $req, $context) {
+    $dir = config('dir');
+    $software_file = $dir.DS.'core'.DS.'config'.DS.'software'.DS.'software.json';
+//    var_dump($software_file);
+    $is_file = file_exists($software_file);
+    $apps = parse_apps($software_file);
+    if($req){
+        $apps = array_filter($apps, function ($app) use ($req) {
+            var_dump($req[0]);
+            return stripos(strtolower($app['Name']), $req[0]) !== false || stripos(strtolower($app['Desc']), $req[0]) !== false;
+        });
+    }
+    return ok('', ['path'=>$dir, 'apps'=>$apps, 'is_file'=>$is_file]);
 });
 // __DIR__ 入口位置
 $dialog = new Dialog(__DIR__);
 $webview->bind('dir', function ($seq, $req, $context) use($dialog){
-    $ret = $dialog->dir(dirs:"D:/");
+    $conf = config();
+    $ret = $dialog->dir(dirs:$conf['dir']);
+    config_save($ret);
     return ok('', ['path'=>$ret]);
 });
 // 运行
